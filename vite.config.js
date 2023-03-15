f3671fd64f31b51766baf917acf646e447cd0bf1
GHOST <ghostdevbusiness@gmail.com>
2023-03-15 17:11:35 +0000

    update template

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    // configure vite for HMR with Gitpod
-    hmr: process.env.GITPOD_WORKSPACE_URL
-      ? {
-        // removes the protocol and replaces it with the port we're connecting to
-        host: process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-'),
-        protocol: 'wss',
-        clientPort: 443
-      }
-      : true
-  }
 })
