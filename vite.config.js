b1699bd389834dcfef75e5343282876af97f8aae
Yushin Washio <yuwash@yandex.com>
2023-11-05 19:08:14 +0000

    Set base URL in vite.config.js (for GitHub pages)

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte()], base: '/jigsaw-puzzle/'
 })
