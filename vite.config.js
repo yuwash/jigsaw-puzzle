be4549b2b899ca21e646c95bb3367e55bdf6d2f9
Yushin Washio <yuwash@yandex.com>
2023-11-11 14:11:33 +0000

    Add vite-plugin-pwa, replacing checked-in manifest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [svelte()], base: '/jigsaw-puzzle/'
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.jpg'],
+      manifest: {
+        name: 'Jigsaw Puzzle',
+        short_name: 'Puzzle',
+        description: 'Simple jigsaw puzzle game.',
+        theme_color: 'brisque',
+        icons: [
+          {
+            src: 'favicon.jpg',
+            sizes: '64x64',
+            type: 'image/jpg'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/jigsaw-puzzle/'
 })
