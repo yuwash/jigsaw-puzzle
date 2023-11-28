981e7f8cdd6c46ef9f052d7bd512b0356717e5e1
Yushin Washio <yuwash@yandex.com>
2023-11-28 23:56:31 +0000

    Enable resizing puzzle area

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@ import Moveable from "svelte-moveable";
 import { writable } from 'svelte/store';
 import { tick } from "svelte";
 export let puzzle, checkTiles, setImage, setImageByUrl, updateGuidelines, rescale, shuffle, solutionMoves, finished;
+let gridTarget, puzzleAreaTarget, puzzleAreaMoveable;
 let moveablesByTile = {};
 
 const tileBoxesFromPuzzle = puzzle => Object.fromEntries(puzzle.shelf.map(
@@ -28,6 +29,15 @@ const onFileChange = event => {
 const onSetExampleImage = () => {
 	setImageByUrl(puzzle, puzzle.exampleImage.url).then(afterRescale);
 }
+const onResizePuzzleArea = ({ detail: e }) => {
+	puzzleAreaTarget.style.width = `${e.width}px`;
+	const gridHeightFactor = (
+		gridTarget.getClientRects()[0].height / puzzleAreaTarget.getClientRects()[0].height
+	);
+	puzzle.height = 100 * gridHeightFactor * e.height / window.innerWidth;
+	rescale(puzzle);
+	afterRescaleNoUpdateRect();
+}
 const resetMoveables = () => {
 	for (const moveable of Object.values(moveablesByTile)) {
 		const manager = moveable.getManager();
@@ -48,6 +58,13 @@ const updateMoveableGuidelines = () => {
 	verticalGuidelines = puzzle.verticalGuidelines;
 }
 const afterRescale = () => {
+	height = puzzle.height;
+	tick().then(() => {
+		updateMoveableGuidelines();
+		puzzleAreaMoveable.updateRect();
+	});
+}
+const afterRescaleNoUpdateRect = () => {
 	height = puzzle.height;
 	tick().then(updateMoveableGuidelines);
 }
@@ -74,31 +91,42 @@ const solvePuzzle = () => {
 		<button type="button" class="button" on:click={shufflePuzzle}>Shuffle</button>
 		<button type="button" class="button" on:click={solvePuzzle}>Solve</button>
 	</p>
-	<div class="grid" id="grid-{puzzle.name}" style="height: {height}vw">
-	{#each puzzle.grid as cell}
-	<div class="grid-cell" id="grid-cell-{puzzle.name}-{cell.name}" style="left: {cell.x * 100}%; top: {cell.y * 100}%; width: {cell.shape.width * 100}%; height: {cell.shape.height * 100}%;"></div>
-	{/each}
-	</div>
-	<div class="shelf" id="shelf-{puzzle.name}" style="height: {height}vw">
-	{#each puzzle.shelf as tile}
-	{@const tileBox = $tileBoxes[tile.name]}
-	<div class="tile" id="tile-{puzzle.name}-{tile.name}" bind:this={puzzle.tileTargets[tile.name]} style="left: {tileBox.x * 100}%; top: {tileBox.y * 100}%; width: {tile.shape.width * 100}%; height: {tile.shape.height * 100}%">
-		<svg style="width: 100%; height: 100%;">
-			<text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">{tile.label}</text>
-		</svg>
-	</div>
-	<Moveable
-		className="tile-movable"
-		target={puzzle.tileTargets[tile.name]}
-		draggable={true}
-		snappable={true}
-		horizontalGuidelines={horizontalGuidelines}
-		verticalGuidelines={verticalGuidelines}
-		snapThreshold={10}
-		on:drag={onDrag}
-		bind:this={moveablesByTile[tile.name]}
-	/>
-	{/each}
+	<div class="puzzle-area" bind:this={puzzleAreaTarget}>
+		<div class="grid" id="grid-{puzzle.name}" style="height: {height}vw" bind:this={gridTarget}>
+			{#each puzzle.grid as cell}
+			<div class="grid-cell" id="grid-cell-{puzzle.name}-{cell.name}" style="left: {cell.x * 100}%; top: {cell.y * 100}%; width: {cell.shape.width * 100}%; height: {cell.shape.height * 100}%;"></div>
+			{/each}
+		</div>
+		<div class="shelf" id="shelf-{puzzle.name}" style="height: {height}vw">
+			{#each puzzle.shelf as tile}
+			{@const tileBox = $tileBoxes[tile.name]}
+			<div class="tile" id="tile-{puzzle.name}-{tile.name}" bind:this={puzzle.tileTargets[tile.name]} style="left: {tileBox.x * 100}%; top: {tileBox.y * 100}%; width: {tile.shape.width * 100}%; height: {tile.shape.height * 100}%">
+				<svg style="width: 100%; height: 100%;">
+					<text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">{tile.label}</text>
+				</svg>
+			</div>
+			<Moveable
+				className="tile-movable"
+				target={puzzle.tileTargets[tile.name]}
+				draggable={true}
+				snappable={true}
+				horizontalGuidelines={horizontalGuidelines}
+				verticalGuidelines={verticalGuidelines}
+				snapThreshold={10}
+				on:drag={onDrag}
+				bind:this={moveablesByTile[tile.name]}
+			/>
+			{/each}
+		</div>
+		<Moveable
+			className="puzzle-area-movable"
+		    target={puzzleAreaTarget}
+		    resizable={true}
+		    keepRatio={true}
+		    renderDirections={["e","s","se"]}
+		    on:resize={onResizePuzzleArea}
+			bind:this={puzzleAreaMoveable}
+		/>
 	</div>
 </main>
 
@@ -110,5 +138,8 @@ const solvePuzzle = () => {
 	.grid-cell {display: inline-block; position: absolute}
 	.grid-cell:hover, .grid-cell:active {background-color:mintcream;}
 	.shelf {position: relative; width: 100%; background-color: aliceblue;}
+	.puzzle-area {position: relative;}
 	:global(.tile-movable .moveable-origin, .tile-movable .moveable-line) {opacity: 0;}
+	:global(.puzzle-area-movable .moveable-origin, .puzzle-area-movable .moveable-line, .puzzle-area-movable .moveable-resizable) {opacity: 0;}
+	:global(.puzzle-area-movable .moveable-resizable:hover, .puzzle-area-movable .moveable-resizable:active) {opacity: 1;}
 </style>
