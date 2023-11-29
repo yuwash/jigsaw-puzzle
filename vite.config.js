c3d8f4c301057bf4efd1a9670f9375594b8f3784
Yushin Washio <yuwash@yandex.com>
2023-11-29 23:55:14 +0000

    Fix pwa issues (add svg icon, use hex color)

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,21 @@ export default defineConfig({
     svelte(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.jpg'],
+      includeAssets: ['favicon.jpg', 'favicon.svg'],
       manifest: {
         name: 'Jigsaw Puzzle',
         short_name: 'Puzzle',
         description: 'Simple jigsaw puzzle game.',
-        theme_color: 'brisque',
+        theme_color: '#ffe4c4', // 'brisque'
         icons: [
           {
             src: 'favicon.jpg',
             sizes: '64x64',
             type: 'image/jpg'
+          },
+          {
+            src: 'favicon.svg',
+            type: 'image/svg+xml'
           }
         ]
       }
